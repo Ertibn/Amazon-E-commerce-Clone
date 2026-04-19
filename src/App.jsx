b81ab4421256c3723589f5b1831506b001fb0080
Ertiban <ertibandbb@gmail.com>
2026-04-19 17:58:45 +0300

    category

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Carousel from './components/carousel/carousel'
+import Category from './components/category/Category'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Carousel />
+          <Category />
+
     </div>
   );
 }
